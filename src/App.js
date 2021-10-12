a2629bbd94ce33f510ede934f3e34c9a53a3d0bf
Sarah Schüßler <stschuessler@googlemail.com>
2021-10-12 20:41:55 +0200

    Creat mock-data.js and import data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import Clothing from './components/Clothing'
 import styled from 'styled-components/macro'
-
-const INITIAL_DATA = [
-  { id: '1', imageUrl: '/images/bluse-floral.png' },
-  { id: '2', imageUrl: '/images/pullover-gruen.png' },
-  { id: '3', imageUrl: '/images/pullover-rosa.png' },
-  { id: '4', imageUrl: '/images/t-shirt-floral.png' },
-  { id: '5', imageUrl: '/images/bluse-floral.png' },
-  { id: '6', imageUrl: '/images/pullover-gruen.png' },
-  { id: '7', imageUrl: '/images/pullover-rosa.png' },
-  { id: '8', imageUrl: '/images/t-shirt-floral.png' },
-]
+import data from './mockup-data'
 
 function App() {
   return (
