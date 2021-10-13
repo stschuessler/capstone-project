e2d60252cfbf96caa596f7d18c3fd2421eda16f3
Sarah Schüßler <stschuessler@googlemail.com>
2021-10-13 14:51:46 +0200

    Implement ternary expression for styled components

--- src/components/Clothing.js
+++ src/components/Clothing.js
@@ -1,12 +1,13 @@
 import { useState } from 'react'
-import styled from 'styled-components/macro'
+import styled, { css } from 'styled-components/macro'
 
 function Clothing({ titel, color, pattern, material, fitting, imageUrl }) {
   const [showDetails, setShowDetails] = useState(false)
 
   return (
-    <section>
-      <button
+    <StyledSection>
+      <StyledButton
+        showDetails={showDetails}
         onClick={() => {
           if (showDetails) {
             setShowDetails(false)
@@ -16,24 +17,41 @@ function Clothing({ titel, color, pattern, material, fitting, imageUrl }) {
         }}
       >
         <StyledImage src={imageUrl} alt="" />
-      </button>
-      {showDetails ? (
-        <div>
-          <h2>{titel}</h2>
-          <p>Farbe: {color}</p>
-          <p>Muster: {pattern}</p>
-          <p>Material: {material}</p>
-          <p>Passform: {fitting}</p>
-        </div>
-      ) : (
-        <div></div>
-      )}
-    </section>
+        {showDetails ? (
+          <div>
+            <h2>{titel}</h2>
+            <p>Farbe: {color}</p>
+            <p>Muster: {pattern}</p>
+            <p>Material: {material}</p>
+            <p>Passform: {fitting}</p>
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </StyledButton>
+    </StyledSection>
   )
 }
 
+const StyledSection = styled.section`
+  overflow: ${({ showDetails }) => (showDetails ? 'hidden' : 'auto')};
+`
+
+const StyledButton = styled.button`
+  ${({ showDetails }) =>
+    showDetails &&
+    css`
+      width: 100vw;
+      height: 100vh;
+      position: fixed;
+      z-index: 1;
+      left: 0;
+      top: 0;
+    `}
+`
+
 const StyledImage = styled.img`
-  width: 200px;
+  width: 100%;
   border-radius: 10px;
   box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
   padding: 2px;
