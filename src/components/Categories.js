169348e176fb1aac11ac15afe4e87a4e529d79cf
Sarah Schüßler <stschuessler@googlemail.com>
2021-10-25 14:55:33 +0200

    Rearrange input and label tags

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,18 +1,48 @@
-function Categories({ categories, onClickExit, uniqueCategories }) {
+function Categories({ categories, onClickExit, uniqueCategories, checkbox }) {
   return (
     <>
       {categories && (
         <section>
-          <input type="checkbox" id="" onClick="myFunction()" />
-          <p id="">{uniqueCategories[0]}</p>
-          <input type="checkbox" id="" onClick="myFunction()" />
-          <p id="">{uniqueCategories[1]}</p>
-          <input type="checkbox" id="" onClick="myFunction()" />
-          <p id="">{uniqueCategories[2]}</p>
+          <form>
+            <div>
+              <StyledLabel>
+                <StyledInput
+                  name={uniqueCategories[0]}
+                  type="checkbox"
+                  value=""
+                  //   checked={checkbox}
+                />
+                {uniqueCategories[0]}
+              </StyledLabel>
 
-          <button>Load</button>
-          <button>Clear</button>
-          <button onClick={() => onClickExit()}>Exit</button>
+              <StyledLabel>
+                <StyledInput
+                  name={uniqueCategories[1]}
+                  type="checkbox"
+                  value=""
+                  //   checked={checkbox}
+                />
+                {uniqueCategories[1]}
+              </StyledLabel>
+
+              <StyledLabel>
+                <StyledInput
+                  name={uniqueCategories[2]}
+                  type="checkbox"
+                  value=""
+                  //   checked={checkbox}
+                />
+                {uniqueCategories[2]}
+              </StyledLabel>
+            </div>
+            <div>
+              <StyledButton>Load</StyledButton>
+              <StyledButton type="reset" value="Reset">
+                Clear
+              </StyledButton>
+              <StyledButton onClick={() => onClickExit()}>Exit</StyledButton>
+            </div>
+          </form>
         </section>
       )}
     </>
